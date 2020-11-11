b83d26608bfde26761a3bdbf78b2a95158a86b07
YEZIHANG <beok02089163@163.com>
2020-11-11 17:54:48 +0800

    自定义校验器与字段验证

--- gin_demo/app/validate/validate.go
+++ gin_demo/app/validate/validate.go
@@ -2,8 +2,11 @@ package validate
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 // SignUpParam validator
@@ -16,10 +19,41 @@ type SignUpParam struct {
 	Email      string `json:"email" binding:"required,email"`
 	Password   string `json:"password" binding:"required"`
 	REPassword string `json:"re_password" binding:"required,eqfield=Password"`
+	Age        string `json:"age" binding:"required,gte=1,lte=130"`
+	Date       string `json:"date" binding:"required,datetime=2006-01-02,checkDate"`
+}
+
+func customFieldValidation(fl validator.FieldLevel) bool {
+	date, err := time.Parse("2006-01-02", fl.Field().String())
+	if err != nil {
+		return false
+	}
+	if date.Before(time.Now()) {
+		return false
+	}
+	return true
+}
+
+// InitValidator 自定义校验器
+func InitValidator() error {
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		if err := v.RegisterValidation("checkDate", customFieldValidation); err != nil {
+			return err
+		}
+		return nil
+	}
+	return nil
 }
 
 // SignUpService handler
 func SignUpService(c *gin.Context) {
+	if err := InitValidator(); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 2001,
+			"msg":  err.Error(),
+		})
+		return
+	}
 	var signs SignUpParam
 	if err := c.ShouldBind(&signs); err != nil {
 		c.JSON(http.StatusOK, gin.H{
