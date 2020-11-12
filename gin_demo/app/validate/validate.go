e8d3c9f1774d3fa7d292cb1c77126c0704cbcdca
YEZIHANG <beok02089163@163.com>
2020-11-12 13:40:16 +0800

    翻译校验器

--- gin_demo/app/validate/validate.go
+++ gin_demo/app/validate/validate.go
@@ -1,12 +1,17 @@
 package validate
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/en"
+	"github.com/go-playground/locales/zh"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	enTranslations "github.com/go-playground/validator/v10/translations/en"
 )
 
 // SignUpParam validator
@@ -23,6 +28,8 @@ type SignUpParam struct {
 	Date       string `json:"date" binding:"required,datetime=2006-01-02,checkDate"`
 }
 
+var trans ut.Translator
+
 func customFieldValidation(fl validator.FieldLevel) bool {
 	date, err := time.Parse("2006-01-02", fl.Field().String())
 	if err != nil {
@@ -37,6 +44,20 @@ func customFieldValidation(fl validator.FieldLevel) bool {
 // InitValidator 自定义校验器
 func InitValidator() error {
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+
+		// 错误信息翻译校验器
+		zhT := zh.New()
+		enT := en.New()
+
+		// 第一个参数表示备用语言环境，后面的参数表示应该支持的语言环境
+		uni := ut.New(enT, zhT, enT)
+		var ok bool
+		trans, ok = uni.FindTranslator("zh")
+		if !ok {
+			return fmt.Errorf("uni.GetTranslator(zh) failed")
+		}
+		enTranslations.RegisterDefaultTranslations(v, trans)
+		// 注册自定义的字段校验器
 		if err := v.RegisterValidation("checkDate", customFieldValidation); err != nil {
 			return err
 		}
@@ -56,9 +77,20 @@ func SignUpService(c *gin.Context) {
 	}
 	var signs SignUpParam
 	if err := c.ShouldBind(&signs); err != nil {
+		errs, ok := err.(validator.ValidationErrors)
+		// 非validator.ValidationErrors类型的错误直接返回
+		if !ok {
+			c.JSON(http.StatusOK, gin.H{
+				"code": 2001,
+				"msg":  err.Error(),
+			})
+			return
+		}
+
+		// validator.ValidationErrors类型的错误翻译后返回
 		c.JSON(http.StatusOK, gin.H{
 			"code": 2001,
-			"msg":  err.Error(),
+			"msg":  errs.Translate(trans),
 		})
 		return
 	}
